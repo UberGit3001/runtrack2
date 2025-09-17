9ab4ee29b755d16b694e1210a2dff5df4f186092
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-09-17 14:41:15 +0200

    afficher le commentaire en bloc de plusieurs lignes

--- Jour01/job02/index.php
+++ Jour01/job02/index.php
@@ -2,5 +2,10 @@
 // Afficher "Hello LaPlateforme!" (commentaire sur une seule ligne)
 echo "Hello LaPlateforme!<br>";
 
+/*
+    Afficher "Hello LaPlateforme!" 
+    Ceci est un commentaire sur plusieurs lignes (bloc)
+*/
+echo "Hello LaPlateforme!";
 
 ?>
