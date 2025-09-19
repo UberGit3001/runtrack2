13329c1da579470978566add5e07dedd55dd73d3
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-09-19 16:22:31 +0200

    retour de la chaine de caractere a l'envers

---
+++ Jour03/job06/index.php
@@ -0,0 +1,17 @@
+<?php
+$str = "Les choses que l'on possède finissent par nous posséder.";
+$reversed = '';
+
+// Trouver la longueur de la chaîne sans utiliser strlen()
+$length = 0;
+while (isset($str[$length])) {
+    $length++;
+}
+
+// Parcourir la chaîne à l'envers
+for ($i = $length - 1; $i >= 0; $i--) {
+    $reversed .= $str[$i];
+}
+
+echo $reversed;
+?>
