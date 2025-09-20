a2adf17456e923c990475485d04719d60aad1748
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-09-20 22:29:43 +0200

    mettre une fonction clearstatcache() pour juste le côté pratique et reset  des tests

--- Jour04/job01/index.php
+++ Jour04/job01/index.php
@@ -11,4 +11,11 @@ foreach ($_GET as $key => $value) {
 echo "Le nombre d’argument GET envoyé est : " . $nb;
 ?>
 
+<form method="get" action="">
+    <input type="text" name="prenom" placeholder="Votre prénom"><br>
+    <input type="text" name="nom" placeholder="Votre nom"><br>
+    <input type="text" name="age" placeholder="Votre âge"><br>
+    <input type="submit" value="Envoyer">
+</form>
+
 
---
+++ Jour04/job02/index.php
@@ -0,0 +1,22 @@
+<?php
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Argument</th><th>Valeur</th></tr>";
+
+// On parcourt $_GET sans fonction système
+foreach ($_GET as $argument => $valeur) {
+    echo "<tr>";
+    echo "<td>" . $argument . "</td>";
+    echo "<td>" . $valeur . "</td>";
+    echo "</tr>";
+
+    // clearstatcache(); je sais que c'est une fonction système mais je voulais l'inclure ici pour effacer le cache à chaque itération et reprendre à zéro
+    
+}
+echo "</table>";
+?>
+<form method="get" action="">
+    <input type="text" name="prenom" placeholder="Votre prénom"><br>
+    <input type="text" name="nom" placeholder="Votre nom"><br>
+    <input type="number" name="age" placeholder="Votre âge"><br>
+    <input type="submit" value="Envoyer">
+</form>
