d84f5861a0ff8610c443a7d611508bfb0e3db100
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-09-23 11:22:15 +0200

    affichage du mot en gras si premiere lettre du mot en majuscule

---
+++ Jour07/job07/index.php
@@ -0,0 +1,62 @@
+<?php
+// Fonction gras : met en <b> les mots commençant par une majuscule
+function gras($str) {
+    $result = "";
+    $word = "";
+    $i = 0;
+
+    while (isset($str[$i])) {
+        $char = $str[$i];
+
+        if ($char != " ") {
+            $word .= $char;
+        } else {
+            // Mot terminé
+            if (isset($word[0]) && $word[0] >= 'A' && $word[0] <= 'Z') {
+                $result .= "<b>" . $word . "</b>";
+            } else {
+                $result .= $word;
+            }
+            $result .= " ";
+            $word = "";
+        }
+        $i++;
+    }
+
+    // Gérer le dernier mot
+    if ($word != "") {
+        if ($word[0] >= 'A' && $word[0] <= 'Z') {
+            $result .= "<b>" . $word . "</b>";
+        } else {
+            $result .= $word;
+        }
+    }
+
+    return $result;
+}
+
+
+?>
+
+<!-- Formulaire -->
+<form method="post">
+    <input type="text" name="str" placeholder="Entrez votre texte">
+    <select name="fonction">
+        <option value="gras">Gras</option>
+        <option value="cesar">César</option>
+        <option value="plateforme">Plateforme</option>
+    </select>
+    <button type="submit">Transformer</button>
+</form>
+
+<?php
+// Traitement du formulaire
+if (isset($_POST['str']) && isset($_POST['fonction'])) {
+    $str = $_POST['str'];
+    $fonction = $_POST['fonction'];
+
+    if ($fonction == "gras") {
+        echo gras($str);
+    } 
+}
+?>
