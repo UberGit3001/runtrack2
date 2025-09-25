2db323eb9cfb92d6f1fe8538f03601f875d6a415
UberGit3001 <mehdi.sebillot@laplateforme.io>
2025-09-25 14:07:50 +0200

    incrementation de coockies de nombres de visite et reset

--- Jour08/job02/index.php
+++ Jour08/job02/index.php
@@ -1,29 +1,40 @@
 <?php
+session_start(); // juste pour le drapeau, le compteur reste dans le cookie
+
 // 1. Vérifier si le cookie existe déjà
 if (!isset($_COOKIE['nbvisites'])) {
-    // Si le cookie n'existe pas encore, on l'initialise à 0
     $nbvisites = 0;
 } else {
-    // Sinon, on récupère sa valeur
-    $nbvisites = $_COOKIE['nbvisites'];
+    $nbvisites = (int)$_COOKIE['nbvisites'];
 }
 
-// 2. Vérifier si le bouton reset a été cliqué
+// 2. Vérifier si reset demandé
 if (isset($_POST['reset'])) {
-    // Remettre le compteur à zéro
     $nbvisites = 0;
-} else {
-    // Sinon, on incrémente (visite normale)
+    setcookie("nbvisites", $nbvisites, time() + 3600);
+    $_SESSION['just_reset'] = true; // drapeau qui dit "ne pas incrémenter tout de suite"
+    header("Location: " . $_SERVER['PHP_SELF']); 
+    exit;
+}
+
+// 3. Incrémenter seulement si ce n’est pas un reset
+if (!isset($_SESSION['just_reset'])) {
     $nbvisites++;
 }
 
-// 3. Mettre à jour le cookie avec la nouvelle valeur
-// durée de vie = 1 heure (3600 secondes)
-setcookie("nbvisites", $nbvisites, time() + 3600);
+// 4. Nettoyer le drapeau pour les prochaines visites
+unset($_SESSION['just_reset']);
 
+// 5. Mettre à jour le cookie
+setcookie("nbvisites", $nbvisites, time() + 3600);
 ?>
-<p>Nombre de visites (cookie) : <?php echo $nbvisites; ?></p>
+<p>Nombre de visites  (cookies): <?php echo $nbvisites; ?></p>
 
 <form method="post">
     <button type="submit" name="reset">Reset</button>
 </form>
+
+<?php
+// debug : voir le contenu du cookie
+var_dump($_COOKIE);
+?>
